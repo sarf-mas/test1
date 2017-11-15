43678def5419283c068637903e9e2e40f10cb7da
sarf-mas <33648865+sarf-mas@users.noreply.github.com>
2017-11-14 19:01:12 -0600

    Add files via upload

---
+++ src/appjava/Appjava.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appjava;
+import java.sql.*;
+/**
+ *
+ * @author SMaster
+ */
+public class Appjava {
+
+   
+    
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "munisa786";
+    public static final String CONN_STRING = "jdbc:mysql://localhost:3306/youtube";
+    public static void main(String[] args) {
+        
+        
+      
+        
+        
+        
+        try {
+           Connection conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+            System.out.println("connected");
+            System.out.println("fuck this shit");
+            System.out.println(PASSWORD);
+
+
+
+          
+      }catch (SQLException e){
+          System.err.println(); 
+      }
+      
+        // TODO code application logic here
+    }
+    
+}
